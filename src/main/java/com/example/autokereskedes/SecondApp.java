e9e0f53b4a91544708d63de25d07cf64933d7f0d
balazsazigazsagos <balazs.kvetko@gmail.com>
2023-11-28 11:50:54 +0100

    SecondApp létrehpzása, hozzá tartozó SecondAppController létrehozása, és az autóhozzáadáshoz szükséges view fxml létrehozása. Fxml file-ban labelek, imageview, buttonok létrehozása, fxID megadása ezek mezőinek létrehozása a Controllerben.

---
+++ src/main/java/com/example/autokereskedes/SecondApp.java
@@ -0,0 +1,4 @@
+package com.example.autokereskedes;
+
+public class SecondApp {
+}
---
+++ src/main/java/com/example/autokereskedes/SecondAppController.java
@@ -0,0 +1,19 @@
+package com.example.autokereskedes;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class SecondAppController {
+    public ImageView img;
+    public Button submitBTN;
+    public Label labelMarka;
+    public Label labelAr;
+    public Label labelModell;
+    public Label labelEvjarat;
+    public Label labelKepLink;
+
+    public void addToAutoker(ActionEvent event) {
+    }
+}
